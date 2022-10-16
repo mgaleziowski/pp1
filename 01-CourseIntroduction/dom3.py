606b7ff423eb6e1ad60a6feddfff996cb3ec442d
Aji <mikizereq@gmail.com>
2022-10-16 17:33:52 +0200

    4

--- 01-CourseIntroduction/dom3.py
+++ 01-CourseIntroduction/dom3.py
@@ -1,2 +1,20 @@
-a=input("podaj slowo ")
-print(a.find(" ",3))
+text="X-DSPAM-Confidence: 0.8475"
+a=""
+i=len(text)-1
+while(i>=0):
+    b=text[i]
+    a+=b
+    i-=1
+pos=a.find(":")
+text2=a[0:pos]
+c=""
+j=len(text2)-1
+while(j>=0):
+    d=text2[j]
+    c+=d
+    j-=1
+print(float(c))
+    
+    
+
+
