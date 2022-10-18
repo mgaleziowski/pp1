2d1624ee818eb0135ca47141239dd02ab0eea4fd
Aji <mikizereq@gmail.com>
2022-10-18 11:41:59 +0200

    m

--- 03-ControlStructures/cs24.py
+++ 03-ControlStructures/cs24.py
@@ -1,1 +1,8 @@
-print()
+for x in range(5):
+    for y in range(x+1):
+        print("*",end="")
+    print()
+for z in range(3,-1,-1):
+    for v in range(z+1):
+        print("*",end="")
+    print()
