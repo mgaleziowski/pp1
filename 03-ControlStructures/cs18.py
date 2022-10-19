876339048650ef3869ebd5864205d2971d871a9e
Aji <mikizereq@gmail.com>
2022-10-19 19:05:41 +0200

    18

--- 03-ControlStructures/cs18.py
+++ 03-ControlStructures/cs18.py
@@ -1,10 +1,9 @@
 a=[1,2,5]
 b=int(input("jaka kwota? "))
 c=0
-x=2
-while(x>=0):
+for x in range(2,-1,-1):
     c=b//a[x]
     b-=(c*a[x])
     if(c>0):
         print(f"{c} razy {a[x]}PLN")
-    x-=1
+
