8837de4c64c2b77455c3a78745b271978d4c5a59
Aji <mikizereq@gmail.com>
2022-10-21 11:46:20 +0200

    1

---
+++ 06-Arrays/ar5.py
@@ -0,0 +1,5 @@
+ar=[2,3,7,5]
+print(len(ar))
+print(ar[1])
+print(ar[len(ar)-2])
+print(ar[0]+ar[len(ar)-1])
---
+++ 06-Arrays/ar6.py
@@ -0,0 +1,7 @@
+ar=[5,8,31,47,2,19]
+sum=0
+count=0
+for x in range(len(ar)):
+    sum+=ar[x]
+    count+=1
+print(sum/count)
