ec862742a945c0ef07d0d973367c03d5870c57e5
s-121-05 <s-121-05@gumiforest.ae.krakow.pl>
2022-12-19 14:40:56 +0100

    hmm

--- 11-ObjectAttributesAndMethods/oam9.py
+++ 11-ObjectAttributesAndMethods/oam9.py
@@ -2,22 +2,23 @@ import random
 
 class Arrays():
     @staticmethod
-    def a_gen(n):
-        b=[]
+    def a(n):
+        array=[]
         for r in range(n):
-            b.append(n)
-        return b
-    def b_gen(l,m,n):
-        b=[]
-        for r in range(l):
-            b.append(random.randint(m,n))
-        return b
-    def c_gen(array,m,n):
+            array.append(n)
+        return array
+    def b(l,m,n):
+        array=[]
+        for i in range(l):
+            array.append(random.randint(m,n))
+        return array
+    def c(array,m,n):
         count=0
         for i in array:
             if(i>=m and i<=n):
                 count+=1
         return count
+
 
 
     
